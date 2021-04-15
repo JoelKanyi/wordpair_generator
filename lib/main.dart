5ec86198e0e93ee9766b8c2ed7f1f08434ea30a6
JoelKanyi <joelkanyi98@gmail.com>
2021-04-15 14:29:53 +0300

    finished second route

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,10 @@ import 'package:wordpair_generator/random_words.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(primaryColor: Colors.pinkAccent),
-      home: RandomWords());
+        theme: ThemeData(primaryColor: Colors.pinkAccent), home: RandomWords());
   }
 }
